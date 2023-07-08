7eab66560ba0b7b0978d9c24a18a04c55ab2ec07
정휘학 <gnlgrkdl@danusys.com>
2023-07-08 13:14:01 +0900

    검색 기능 추가

--- src/components/units/board/list/BoardList.styles.ts
+++ src/components/units/board/list/BoardList.styles.ts
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { ITextTokenProps } from "./BoardList.types";
+import type { ITextTokenProps } from "./BoardList.types";
 
 export const Wrapper = styled.div`
   width: 1200px;
